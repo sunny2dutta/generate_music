aefb6341798a78fe0c14c001db5f29e434bfed10
sunny2dutta <sunn2dutta@gmail.com>
2023-06-06 19:32:25 +0400

    files

--- mit_6.S191/image_encode_decode.py
+++ mit_6.S191/image_encode_decode.py
@@ -8,23 +8,34 @@ def build_autoencoder(image,code_size):
     encoder = tf.keras.Sequential([
         tf.keras.layers.InputLayer(img_shape),
         # Convolutional layer with 64 filters of size 3x3 and ReLU activation
-        tf.keras.layers.Conv2D(64, kernel_size=3, activation='relu',padding='same'),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
         # Max pooling layer with pool size of 2x2 and stride of 2
-        tf.keras.layers.MaxPool2D(pool_size=2,padding='same'),
+        tf.keras.layers.MaxPool2D((2, 2),padding='same'),
+        tf.keras.layers.Dropout(0.3),
 
         # Convolutional layer with 128 filters of size 3x3 and ReLU activation
-        tf.keras.layers.Conv2D(8, kernel_size=3, activation='relu',padding='same'),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
         # Max pooling layer with pool size of 2x2 and stride of 2
-        tf.keras.layers.MaxPool2D(pool_size=2,padding='same'),
+        tf.keras.layers.MaxPool2D((2,2),padding='same'),
+        tf.keras.layers.Dropout(0.2),
+
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
+        # Max pooling layer with pool size of 2x2 and stride of 2
+        tf.keras.layers.MaxPool2D((2,2),padding='same'),
+
+        
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(code_size)
     ])
     
     decoder = tf.keras.Sequential([
-        tf.keras.layers.InputLayer(code_size,),
+        tf.keras.layers.InputLayer((code_size,)),
         tf.keras.layers.Dense(np.prod(img_shape)),
-        tf.keras.layers.Reshape(img_shape)
-    ])
+        tf.keras.layers.Reshape(img_shape),
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+        tf.keras.layers.Conv2D(1, (2, 2), activation='relu', padding='same')
+        ])
     return encoder,decoder
 
 
